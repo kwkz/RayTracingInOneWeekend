d1a73a053be2e80cf80f8130a72bd41364d46783
kwkz <kwkz@users.noreply.github.com>
2020-08-18 19:15:27 +0900

    pngで書きだすようにした

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,15 +1,24 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 #include <iostream>
 
 int main() {
-  
+  constexpr char filename[] = "image.png";
+
   // image
 
   constexpr int64_t image_width = 256;
   constexpr int64_t image_height = 256;
 
-  // render
+  struct pixel {
+    pixel() = default;
+    unsigned char r{}, g{}, b{}, a{ 255 };
+  };
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  std::unique_ptr<pixel[][image_width]> data(new pixel[image_height][image_width]);
+
+  // render
 
   for (int j = image_height - 1; j >= 0; --j) {
     for (int i = 0; i < image_width; ++i) {
@@ -17,11 +26,12 @@ int main() {
       auto  g = static_cast<double>(j) / (image_height - 1);
       auto  b = 0.25;
 
-      auto ir = static_cast<int>(255.999 * r);
-      auto ig = static_cast<int>(255.999 * g);
-      auto ib = static_cast<int>(255.999 * b);
+      auto& p = data[(image_height - 1) - j][i];
+      p.r = static_cast<int>(255.999 * r);
+      p.g = static_cast<int>(255.999 * g);
+      p.b = static_cast<int>(255.999 * b);
 
-      std::cout << ir << ' ' << ig << ' ' << ib  << '\n';
     }
   }
+  stbi_write_png(filename, static_cast<int>(image_width), static_cast<int>(image_height), static_cast<int>(sizeof(pixel)), data.get(), 0); 
 }
