b7dbed80ec823a280b1cf39bed524f7b86689143
kwkz <kwkz@users.noreply.github.com>
2020-08-19 21:53:09 +0900

    イメージデータの作成方法を修正

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,7 +1,11 @@
+#include "color.h"
+#include "vec3.h"
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 #include <iostream>
+#include <vector>
 
 int main() {
   constexpr char filename[] = "image.png";
@@ -13,28 +17,32 @@ int main() {
 
   struct pixel {
     pixel() = default;
+    pixel(unsigned char r_, unsigned char g_, unsigned char b_, unsigned char a_  = 255) : r(r_), g(g_), b(b_), a(a_) {}
     unsigned char r{}, g{}, b{}, a{ 255 };
   };
 
-  std::unique_ptr<pixel[][image_width]> data(new pixel[image_height][image_width]);
+  std::vector<pixel> image;
 
   // render
+
+  auto to_pixel = [](const color& c) {
+    return pixel{
+      static_cast<unsigned char>(255.999 * c.x()),
+      static_cast<unsigned char>(255.999 * c.y()),
+      static_cast<unsigned char>(255.999 * c.z()) };
+  };
 
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\nScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      auto  r = static_cast<double>(i) / (image_width - 1);
-      auto  g = static_cast<double>(j) / (image_height - 1);
-      auto  b = 0.25;
-
-      auto& p = data[(image_height - 1) - j][i];
-      p.r = static_cast<int>(255.999 * r);
-      p.g = static_cast<int>(255.999 * g);
-      p.b = static_cast<int>(255.999 * b);
-
+      const color pixel_color(
+        static_cast<double>(i) / (image_width - 1),
+        static_cast<double>(j) / (image_height - 1),
+        0.25);
+      image.emplace_back(to_pixel(pixel_color));
     }
   }
-  stbi_write_png(filename, static_cast<int>(image_width), static_cast<int>(image_height), static_cast<int>(sizeof(pixel)), data.get(), 0); 
+  stbi_write_png(filename, static_cast<int>(image_width), static_cast<int>(image_height), static_cast<int>(sizeof(pixel)), image.data(), 0);
   
   std::cerr << "\nDone\n";
 }
