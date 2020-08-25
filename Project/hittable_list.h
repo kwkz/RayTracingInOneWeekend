67f91a74a7dc7dbbb27757b267d41ff41052b263
kwkz <kwkz@users.noreply.github.com>
2020-08-25 10:50:51 +0900

    チャプター6.5を追加。複数オブジェクトとの交差をとるため準備。

---
+++ Project/hittable_list.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#ifndef HITTABLE_LIST_H_INCLUDED
+#define HITTABLE_LIST_H_INCLUDED
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+class hittable_list : public hittable {
+public:
+  hittable_list() = default;
+  hittable_list(std::shared_ptr<hittable> object) { add(object); }
+
+  ~hittable_list() = default;
+
+  void clear() { objects_.clear(); }
+  void add(std::shared_ptr<hittable> object) { objects_.emplace_back(object); }
+
+  virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+public:
+  std::vector<std::shared_ptr<hittable>> objects_;
+};
+
+bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+  hit_record temp_rec;
+  bool hit_anything = false;
+  auto closest_so_far = t_max;
+
+  for (const auto& object : objects_) {
+    if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+      hit_anything = true;
+      closest_so_far = temp_rec.t;
+      rec = temp_rec;
+    }
+  }
+  return hit_anything;
+}
+#endif // !HITTABLE_LIST_H_INCLUDED
---
+++ Project/sphere.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#ifndef SPHERE_H_INCLUDED
+#define SPHERE_H_INCLUDED
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+public:
+  sphere() = default;
+  sphere(point3 center, double radius) noexcept : center_(center), radius_(radius) {}
+
+  ~sphere() = default;
+
+  virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+public:
+  point3 center_;
+  double radius_;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+  vec3 oc = r.origin() - center_;
+  auto a = r.direction().length_squared();
+  auto half_b = dot(oc, r.direction());
+  auto c = oc.length_squared() - radius_ * radius_;
+  auto discriminant = half_b * half_b - a * c;
+
+  if (discriminant > 0) {
+    auto root = sqrt(discriminant);
+
+    auto temp = (-half_b - root) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.at(rec.t);
+
+      vec3 outward_normal = (rec.p - center_) / radius_;
+      rec.set_face_normal(r, outward_normal);
+      return true;
+    }
+
+    temp = (-half_b + root) / a;
+    if (temp < t_max && temp > t_min) {
+      rec.t = temp;
+      rec.p = r.at(rec.t);
+
+      vec3 outward_normal = (rec.p - center_) / radius_;
+      rec.set_face_normal(r, outward_normal);
+      return false;
+    }
+  }
+  return false;
+}
+
+#endif // !SPHERE_H_INCLUDED
