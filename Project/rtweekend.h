ae1e8d6f14a4ba1aa9ed6ccb3e1bc8a5e7a80802
kwkz <kwkz@users.noreply.github.com>
2020-09-06 21:07:09 +0900

    チャプター6.7を追加

---
+++ Project/rtweekend.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#ifndef RTWEEKEND_H_INCLUDED
+#define RTWEEKEND_H_INCLUDED
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+// Usings
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+constexpr double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline constexpr double degree_to_radians(double degrees) {
+  return degrees * pi / 180.0;
+}
+
+// Common Headers
+#include "ray.h"
+#include "vec3.h"
+
+#endif
