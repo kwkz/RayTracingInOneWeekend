794a0388a756abec3a7b03e3e1ea4e72c9797d17
kwkz <kwkz@users.noreply.github.com>
2021-02-10 18:23:42 +0900

    処理を書籍に合わせた

--- Project/sphere.h
+++ Project/sphere.h
@@ -8,48 +8,44 @@
 class sphere : public hittable {
 public:
   sphere() = default;
-  sphere(point3 center, double radius) noexcept : center_(center), radius_(radius) {}
+  sphere(point3 center, double radius) noexcept
+      : center_(center), radius_(radius) {}
 
   ~sphere() = default;
 
-  virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+  virtual bool hit(const ray &r, double t_min, double t_max,
+                   hit_record &rec) const override;
 
 public:
   point3 center_;
   double radius_;
 };
 
-bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+bool sphere::hit(const ray &r, double t_min, double t_max,
+                 hit_record &rec) const {
   vec3 oc = r.origin() - center_;
   auto a = r.direction().length_squared();
   auto half_b = dot(oc, r.direction());
   auto c = oc.length_squared() - radius_ * radius_;
+
   auto discriminant = half_b * half_b - a * c;
+  if (discriminant < 0)
+    return false;
 
-  if (discriminant > 0) {
-    auto root = sqrt(discriminant);
-
-    auto temp = (-half_b - root) / a;
-    if (temp < t_max && temp > t_min) {
-      rec.t = temp;
-      rec.p = r.at(rec.t);
-
-      vec3 outward_normal = (rec.p - center_) / radius_;
-      rec.set_face_normal(r, outward_normal);
-      return true;
-    }
-
-    temp = (-half_b + root) / a;
-    if (temp < t_max && temp > t_min) {
-      rec.t = temp;
-      rec.p = r.at(rec.t);
-
-      vec3 outward_normal = (rec.p - center_) / radius_;
-      rec.set_face_normal(r, outward_normal);
+  auto sqrtd = sqrt(discriminant);
+  auto root = (-half_b - sqrtd) / a;
+  if (root < t_min || t_max < root) {
+    auto root = (-half_b + sqrtd) / a;
+    if (root < t_min || t_max < root)
       return false;
-    }
   }
-  return false;
+
+  rec.t = root;
+  rec.p = r.at(rec.t);
+  auto outward_normal = (rec.p - center_) / radius_;
+  rec.set_face_normal(r, outward_normal);
+
+  return true;
 }
 
 #endif // !SPHERE_H_INCLUDED
